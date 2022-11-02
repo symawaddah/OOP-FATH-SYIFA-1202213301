f9ed035e028a60ed54cb1ef88de6f79e37576ffc
symawaddah <syifamawaddah@student.telkomuniversity.ac.id>
2022-11-02 16:30:59 +0700

    feat: modul1

---
+++ modul 1/Database.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Database{
+    // TODO Create List of Menu Object to Store Menu from Menu Class
+    public static void main(String[] args){
+        System.out.println("Selamat datang di restoran EAD");
+        System.out.println("Silhahkan Register terlebih dahulu");
+        System.out.println("==========================");
+
+        String Nama, No_handphone;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Nama: ");
+        Nama = input.nextLine();
+        System.out.print("No handphone: ");
+        No_handphone = input.nextLine();
+    }
+
+    // TODO Create Method to Insert Menu to Database
+
+    // TODO Create Method to Show Menu from Database
+
+    // TODO Create Method to Search Menu from Database
+
+
+}
